81dce579bcc046a75d38ef482a5072ea333f9ca1
Alexander Kowalski <25545728+alexkowalski@users.noreply.github.com>
2020-06-18 17:36:16 +0200

    Get banner and version string directly from w2dynamics

--- python/w2dyn_cthyb/__init__.py
+++ python/w2dyn_cthyb/__init__.py
@@ -3,30 +3,20 @@ from triqs.utility import mpi
 from .solver import Solver
 from .converters import get_test_impurity_model,NO_to_Nos,triqs_gf_to_w2dyn_ndarray_g_tosos_beta_ntau,triqs_gf_to_w2dyn_ndarray_g_wosos_beta_niw,generate_testimpurity_with_triqs
 
+from w2dyn.auxiliaries import BANNER, CODE_VERSION, CODE_VERSION_STRING, CODE_DATE, OUTPUT_VERSION
+
 
 __all__ = ['Solver','get_test_impurity_model','NO_to_Nos','triqs_gf_to_w2dyn_ndarray_g_tosos_beta_ntau','triqs_gf_to_w2dyn_ndarray_g_wosos_beta_niw','generate_testimpurity_with_triqs']
 
-BANNER = r"""
-           ______
-         _/XXXXXX\ ___ __  ____   /|            W2DYNAMICS - Wuerzburg/Wien strong
-  |\    | |X/  \X| __ \\ \/ /  \ | |                 coupling impurity solver
-  \ \ |\| |____/X| | \ \\  /| \ \| |
-   \ \\ \ /XXXXXX| |_/ // / | |\ \ |   AUTHORS: M Wallerberger, A Hausoel, P Gunacker, A Kowalski, 
-    \__\__|X/____|____//_/  |_| \__|            N Parragh, F Goth, K Held and G Sangiovanni
-         |XXXXXXXX|                    VERSION: %s (%s)
 
+INTERFACE_BANNER_ADD = r"""
 
-                    &----------------------------------------------------&
-                    | IF W2DYN WAS OF USE FOR A PUBLICATION, PLEASE CITE |
-                    |      https://doi.org/10.1016/j.cpc.2018.09.007     |
-                    |          https://arxiv.org/abs/1807.00361          |
-                    &----------------------------------------------------&
+             &----------------------------------------------------&
+             | IF W2DYN WAS OF USE FOR A PUBLICATION, PLEASE CITE |
+             |      https://doi.org/10.1016/j.cpc.2018.09.007     |
+             |          https://arxiv.org/abs/1807.00361          |
+             &----------------------------------------------------&
 
 """
 
-CODE_VERSION = 1, 0, "0"
-CODE_VERSION_STRING = ".".join(map(str,CODE_VERSION))
-CODE_DATE = "July 2018"
-OUTPUT_VERSION = 2, 2
-
-if mpi.is_master_node(): print(BANNER % (CODE_VERSION_STRING, CODE_DATE))
+if mpi.is_master_node(): print((BANNER + INTERFACE_BANNER_ADD) % (CODE_VERSION_STRING, CODE_DATE))
